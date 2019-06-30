b701c077f1f25313f58e85c6e8dc2f2fc0ea5342
Chikashi Miyama <chikashimiyama@gmail.com>
2019-06-30 14:34:32 +0200

    add first unit test

---
+++ CylVision/Assets/Scripts/UnitTest/SimpleTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using NSubstitute;
+    
+namespace UnitTests
+{
+    public class SimpleTest
+    {
+        [Test]
+        public void FirstTest()
+        {
+            Assert.AreEqual(10, 10);
+        }
+
+        public interface ISomeInterface
+        {
+            void doSomething();
+        }
+        
+        [Test]
+        public void FirstMock()
+        {           
+            var mock = Substitute.For<ISomeInterface>();
+            mock.doSomething();
+            
+            mock.Received(1).doSomething();
+        }
+        
+  
+    }
+
+}
+
