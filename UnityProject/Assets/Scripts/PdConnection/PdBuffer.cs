4f25e1d107be29a0e529975b9abdb4a0a9e373c6
Chikashi Miyama <chikashimiyama@gmail.com>
2019-07-06 16:22:52 +0200

    add PdBuffer

---
+++ UnityProject/Assets/Scripts/PdConnection/PdBuffer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+
+namespace PdConnection
+{
+    public class PdBuffer : IDisposable
+    {
+        private readonly MemoryMappedFile memoryMappedFile_;
+        private readonly MemoryMappedViewAccessor viewAccessor_;
+    
+        public PdBuffer()
+        {
+            memoryMappedFile_ = MemoryMappedFile.OpenExisting("shared_memory");
+            viewAccessor_ = memoryMappedFile_.CreateViewAccessor();
+        }
+        
+        public void Dispose()
+        {
+            viewAccessor_?.Dispose();
+            memoryMappedFile_?.Dispose();
+        }
+    }
+
+}
