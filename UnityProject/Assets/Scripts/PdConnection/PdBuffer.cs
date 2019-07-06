15fbeae0f61bbc25a4a84f45763f3ed9c73c9526
Chikashi Miyama <chikashimiyama@gmail.com>
2019-07-06 16:26:14 +0200

    add scripts

--- UnityProject/Assets/Scripts/PdConnection/PdBuffer.cs
+++ UnityProject/Assets/Scripts/PdConnection/PdBuffer.cs
@@ -5,13 +5,24 @@ namespace PdConnection
 {
     public class PdBuffer : IDisposable
     {
+        public float[] Data { get; }
+
         private readonly MemoryMappedFile memoryMappedFile_;
         private readonly MemoryMappedViewAccessor viewAccessor_;
-    
-        public PdBuffer()
+        
+        public PdBuffer(string name, int size)
         {
-            memoryMappedFile_ = MemoryMappedFile.OpenExisting("shared_memory");
+            memoryMappedFile_ = MemoryMappedFile.OpenExisting(name);
+            if (memoryMappedFile_ == null)
+                throw new ArgumentException("no such shared memory");
+            
             viewAccessor_ = memoryMappedFile_.CreateViewAccessor();
+            Data = new float[size];
+        }
+
+        public void Update()
+        {
+            viewAccessor_.ReadArray(0, Data, 0, Data.Length);
         }
         
         public void Dispose()
