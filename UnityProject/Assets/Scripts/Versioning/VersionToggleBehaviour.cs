e890a6cfd210cdaee9266807be1aa3d233796cfd
Chikashi Miyama <chikashimiyama@gmail.com>
2019-07-13 20:20:29 +0200

    versioning

---
+++ UnityProject/Assets/Scripts/Versioning/VersionToggleBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VersionToggleBehaviour : MonoBehaviour
+{
+    #pragma warning disable 0649
+    [SerializeField] private Canvas versionCanvas;
+    [SerializeField] private Text versionText;
+    [SerializeField] private TextAsset buildNumberFile;
+    #pragma warning restore 0649
+
+    void Start()
+    {
+        versionText.text = "Build: " + buildNumberFile.text;
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            versionCanvas.enabled = !versionCanvas.enabled;
+        }
+    }
+}
