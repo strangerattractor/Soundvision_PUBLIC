cd6e0b64cf20ca2d0fcf0ad1712f088bc8e7f78a
Chikashi Miyama <chikashimiyama@gmail.com>
2019-08-17 17:47:01 +0200

    add example1

---
+++ UnityProject/Assets/Scenes/Examples/Example1/script/BallMapping.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Scenes.Examples.Example1.script
+{
+    public class BallMapping : MonoBehaviour
+    {
+        [SerializeField] private GameObject ball;
+        [SerializeField] private float mapToSize = 10f;
+        [SerializeField] private float mapToColor = 0f;
+        [SerializeField] private RmsAnalyzer analyzer;
+
+        private Renderer renderer_;
+        
+        void Start()
+        {
+            renderer_ = GetComponent<Renderer>();
+            
+        }
+        
+        void Update()
+        {
+            var currentRms = analyzer.RMS;
+            var scale = currentRms * mapToSize;
+            var red = Mathf.Clamp(currentRms * mapToColor, 0f, 1f);
+            
+            ball.transform.localScale = new Vector3(scale, scale, scale);
+            renderer_.material.color = new Color(red, 1f - red, 0f);
+        }
+    }
+}
---
+++ UnityProject/Assets/Scenes/Examples/Example1/script/RmsAnalyzer.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Scenes.Examples.Example1.script
+{
+    public class RmsAnalyzer : MonoBehaviour
+    {
+        [SerializeField] private AudioSource audioSource = default;
+        private const int BufSize = 256;
+        private float[] audioSamples_;
+        private float rms_;
+
+        public float RMS => rms_;
+
+        private void Start()
+        {
+            audioSamples_ = new float[BufSize];
+        }
+
+        private void Update()
+        {
+            audioSource.GetOutputData(audioSamples_, 0);
+            rms_ = GetRms(audioSamples_);
+        }
+
+        private static float GetRms(float[] buffer)
+        {
+            return Mathf.Sqrt(buffer.Sum(sample => sample * sample) / BufSize);
+        }
+    }
+}
