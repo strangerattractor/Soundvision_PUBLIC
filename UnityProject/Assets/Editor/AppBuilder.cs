516e890c00f381272099fb74d22e4cf19755f0af
Chikashi Miyama <chikashimiyama@gmail.com>
2019-10-02 00:26:57 +0200

    fix path

--- UnityProject/Assets/Editor/AppBuilder.cs
+++ UnityProject/Assets/Editor/AppBuilder.cs
@@ -4,7 +4,7 @@ public class AppBuilder
 {
     public static void Build()
     {
-        string[] scenes = { "Assets/Scenes/PdBackendDemo.unity" };
+        string[] scenes = { "Assets/Scenes/examples/PdBackendDemo/PdBackendDemo.unity" };
 
         var args = System.Environment.GetCommandLineArgs();
         BuildPipeline.BuildPlayer(scenes, "../bin/soundvision.exe" , BuildTarget.StandaloneWindows64, BuildOptions.None);
