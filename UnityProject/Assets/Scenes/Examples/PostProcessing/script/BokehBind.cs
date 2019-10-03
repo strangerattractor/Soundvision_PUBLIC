2d55fc6f882ca56cc8c87ec2090b52e59d1833e7
Chikashi Miyama <chikashimiyama@gmail.com>
2019-10-03 21:41:34 +0200

    add interactive rendering of post process

---
+++ UnityProject/Assets/Scenes/Examples/PostProcessing/script/BokehBind.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class BokehBind : MonoBehaviour
+{
+    [SerializeField] private PostProcessVolume volume;
+
+    private DepthOfField depthOfField_;
+    
+    private void Start()
+    {
+        volume.profile.TryGetSettings(out depthOfField_);
+    }
+
+    public void OnEnergyChanged(float energy)
+    {
+        depthOfField_.focusDistance.value = 0.1f + energy / 100f;
+    }
+}
