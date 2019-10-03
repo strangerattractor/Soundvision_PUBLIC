bcc5a0b39d2f4ecc27aa7cddd5311215fc4583fe
Chikashi Miyama <chikashimiyama@gmail.com>
2019-10-03 11:43:11 +0200

    add visual effect graph demo

---
+++ UnityProject/Assets/Scenes/Examples/VisualEffectGraph/script/FlareBind.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class FlareBind : MonoBehaviour
+{
+    [SerializeField] private VisualEffect flareEffect;
+    private static readonly string Spread = "spread";
+    
+    public void OnEnergyChanged(float energy)
+    {
+        flareEffect.SetFloat(Spread, energy * 0.01f );
+    }
+}
---
+++ UnityProject/Assets/Scenes/Examples/VisualEffectGraph/script/SmokeBind.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class SmokeBind : MonoBehaviour
+{
+    [SerializeField] private VisualEffect smokeEffect;
+    private static readonly string SmokeAnimSpeed = "smokeAnimSpeed";
+    
+    public void OnEnergyChanged(float energy)
+    {
+        smokeEffect.SetFloat(SmokeAnimSpeed, energy * 0.1f + 8f);
+    }
+}
