8d8e554556ba350b6ca0709f35a7c49297cac77f
Chikashi Miyama <chikashimiyama@gmail.com>
2019-10-04 19:17:11 +0200

    fix mapping demo

--- UnityProject/Assets/Scenes/Examples/Composition/script/BallMapping.cs
+++ UnityProject/Assets/Scenes/Examples/Composition/script/BallMapping.cs
@@ -6,25 +6,14 @@ namespace Scenes.Examples.Example1.script
     {
         [SerializeField] private GameObject ball;
         [SerializeField] private float mapToSize = 10f;
-        [SerializeField] private float mapToColor = 0f;
         [SerializeField] private RmsAnalyzer analyzer;
-
-        private Renderer renderer_;
-        
-        void Start()
-        {
-            renderer_ = GetComponent<Renderer>();
-            
-        }
         
         void Update()
         {
             var currentRms = analyzer.RMS;
             var scale = currentRms * mapToSize;
-            var red = Mathf.Clamp(currentRms * mapToColor, 0f, 1f);
             
             ball.transform.localScale = new Vector3(scale, scale, scale);
-            renderer_.material.color = new Color(red, 1f - red, 0f);
         }
     }
 }
