e96e6579d5a9f76c956cfc204d5387ed47f0e71c
Chikashi Miyama <chikashimiyama@gmail.com>
2019-10-04 20:15:15 +0200

    fix post script demo

--- UnityProject/Assets/Scenes/Examples/PostProcessing/script/BokehBind.cs
+++ UnityProject/Assets/Scenes/Examples/PostProcessing/script/BokehBind.cs
@@ -1,19 +1,19 @@
 ﻿using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.Experimental.Rendering.HDPipeline;
+using UnityEngine.Rendering;
 
 public class BokehBind : MonoBehaviour
 {
-    [SerializeField] private PostProcessVolume volume;
-
-    private DepthOfField depthOfField_;
+    private Bloom bloom_;
+    [SerializeField] Volume volume;
     
     private void Start()
     {
-        volume.profile.TryGetSettings(out depthOfField_);
+        volume.profile.TryGet(out bloom_);
     }
 
     public void OnEnergyChanged(float energy)
     {
-        depthOfField_.focusDistance.value = 0.1f + energy / 100f;
+        bloom_.intensity.value = energy / 500f;
     }
 }
