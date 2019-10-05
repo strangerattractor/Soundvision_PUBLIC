f90b2dac285b6ae54a9c87897d5e93d403e6b9c6
Chikashi Miyama <chikashimiyama@gmail.com>
2019-10-05 20:15:26 +0200

    add some shader graph demo

--- UnityProject/Assets/Scenes/Examples/Texture/TextureMapper.cs
+++ UnityProject/Assets/Scenes/Examples/Texture/TextureMapper.cs
@@ -11,21 +11,27 @@ namespace cylvester
 
         [SerializeField] private GameObject waveformPanel;
         [SerializeField] private GameObject spectrogramPanel;
+        [SerializeField] private GameObject chikashiPanel;
+
         
         private Renderer waveFormRenderer_;
         private Renderer spectroGramRenderer_;
+        private Renderer chikashiRenderer_;
         private static readonly int baseColorMap_ = Shader.PropertyToID("_BaseColorMap");
+        private static readonly int myTexture_ = Shader.PropertyToID("_MyTexture");
         
         void Start()
         {
             waveFormRenderer_ = waveformPanel.GetComponent<Renderer>();
             spectroGramRenderer_ = spectrogramPanel.GetComponent<Renderer>();
+            chikashiRenderer_ = chikashiPanel.GetComponent<Renderer>();
         }
 
         void Update()
         {
             waveFormRenderer_.material.SetTexture(baseColorMap_, waveform.Texture);
             spectroGramRenderer_.material.SetTexture(baseColorMap_, spectrogram.Texture);
+            chikashiRenderer_.material.SetTexture(myTexture_, waveform.Texture);
         }
     }
 }
