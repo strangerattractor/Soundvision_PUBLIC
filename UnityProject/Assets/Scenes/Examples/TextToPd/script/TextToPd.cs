abc4c19877f2f27bdf6b54db715d2eb17d9f41af
Chikashi Miyama <chikashimiyama@gmail.com>
2019-10-27 17:28:00 +0100

    change the format of UDP communication from Unity to Pd to ascii

---
+++ UnityProject/Assets/Scenes/Examples/TextToPd/script/TextToPd.cs
@@ -0,0 +1,12 @@
+﻿using cylvester;
+using UnityEngine;
+
+public class TextToPd : MonoBehaviour
+{
+    [SerializeField] private PdBackend pdBackend;
+    
+    void Start()
+    {
+        pdBackend.Message("Hello world\n");
+    }
+}
