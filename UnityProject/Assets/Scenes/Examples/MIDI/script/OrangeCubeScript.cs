f0561e44c953673b531bfa92649f5a5cb223315f
max <mschweder@gmail.com>
2019-10-31 18:37:03 +0100

    Animator Tryout

---
+++ UnityProject/Assets/Scenes/Examples/MIDI/script/OrangeCubeScript.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrangeCubeScript : MonoBehaviour
+{
+    public bool go;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Go()
+{
+    go = true;
+}
+
+    private void Update()
+    {
+        if (go == true)
+        {
+                gameObject.GetComponent<Animator>().SetTrigger("AnimTrigger");
+        }
+    }
+    
+    
+}
