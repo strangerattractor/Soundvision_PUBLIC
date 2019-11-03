f28b61979d577bc927dff4e8b6f6f0883df3ac64
Chikashi Miyama <chikashimiyama@gmail.com>
2019-11-03 23:37:01 +0100

    add kinect point cloud

---
+++ UnityProject/Assets/KinectPointCloud.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+namespace cylvester
+{
+    public class KinectPointCloud : MonoBehaviour
+    {
+        [SerializeField] private VisualEffect visualEffect;
+
+        private Texture2D cachedTexture_;
+        private Texture2D data_;
+        public void Start()
+        {
+            cachedTexture_  = new Texture2D(512, 424, TextureFormat.R16, false);
+            data_ = new Texture2D(512, 424, TextureFormat.R16, false);
+        }
+
+        public void Update()
+        {
+            //Graphics.CopyTexture(data_, cachedTexture_);
+        }
+        
+        public void OnInfraredFrameReceived( Texture2D data)
+        {
+            data_ = data;
+
+            visualEffect.SetTexture("DepthImage", data_);
+            visualEffect.SetTexture("CachedTexture", cachedTexture_);
+        }
+    }
+
+}
+
