a96f8ce09f4640f3201246ec0fbb051acfdec6b9
Chikashi Miyama <chikashimiyama@gmail.com>
2019-11-03 18:27:55 +0100

    add particle emission from right hand

--- UnityProject/Assets/Scenes/Examples/KinectSkeleton/script/RightHandMarker.cs
+++ UnityProject/Assets/Scenes/Examples/KinectSkeleton/script/RightHandMarker.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 namespace cylvester
 {
     public class RightHandMarker : MonoBehaviour
     {
+        private float previousCallback_;
+        
         public void OnJointDataReceived(Windows.Kinect.Joint joint)
         {
             transform.position = new Vector3(joint.Position.X * 10f, joint.Position.Y * 10f, joint.Position.Z * 10f);
