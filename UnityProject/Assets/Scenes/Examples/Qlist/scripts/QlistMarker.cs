027bfcc1325016b91167c5b67fd1b75ed4e4059b
Chikashi Miyama <chikashimiyama@gmail.com>
2019-11-30 11:40:15 +0100

    add qlist transition system

---
+++ UnityProject/Assets/Scenes/Examples/Qlist/scripts/QlistMarker.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace cylvester
+{
+    [CustomStyle("Annotation")]
+    public class QlistMarker : Marker, INotification
+    {
+        [TextArea] public string stateName;
+
+        public PropertyName id => stateName;
+    }
+}
---
+++ UnityProject/Assets/Scenes/Examples/Qlist/scripts/TimelineController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+namespace cylvester
+{
+    public class TimelineController : MonoBehaviour, INotificationReceiver
+    {
+        [SerializeField] private PlayableDirector playableDirector;
+        [SerializeField] private StateManager stateManager;
+        
+        private IList<QlistMarker> qlistMarkers_;
+        
+        public void Start()
+        { 
+            var timeline = (TimelineAsset)playableDirector.playableAsset;
+            var markerTrack = timeline.markerTrack;
+            var markers = markerTrack.GetMarkers();
+            qlistMarkers_ = new Boo.Lang.List<QlistMarker>();
+            foreach (var marker in markers)
+                qlistMarkers_.Add((QlistMarker)marker);
+        }
+        
+        public void OnStateChanged(IStateReader stateManager)
+        {
+            var stateName = stateManager.CurrentState.Title;
+            foreach (var qlistMarker in qlistMarkers_)
+            {
+                if (qlistMarker.id != stateName) continue;
+                playableDirector.Stop();
+                playableDirector.time = qlistMarker.time;
+                playableDirector.Play();
+                break;
+            }
+        }
+
+        public void OnNotify(Playable origin, INotification notification, object context)
+        {
+            if (!stateManager.NextState.HasValue)
+                return;
+
+            var nextState = stateManager.NextState.Value;
+            if(notification.id == nextState.Title)
+                playableDirector.Pause(); // reaches the next state (marker) in timeline
+        }
+    }
+}
