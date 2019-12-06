69a078a9211198d5b3be05fc0041ecc2971e066e
max <mschweder@gmail.com>
2019-12-06 11:25:38 +0100

    Insta Trig Fixed

--- UnityProject/Assets/Scripts/PdConnection/MidiTransitionController.cs
+++ UnityProject/Assets/Scripts/PdConnection/MidiTransitionController.cs
@@ -13,17 +13,16 @@ namespace cylvester
 
         private int oneBarLoop = 96;
         private int fourBarLoop = 384;
-        private int instaTrig = 2;
-        private int nextSelectedSceneOutput = 18;
-        private int lastSelectedSceneOutput = 17;
+        private const int cmd_instaTrig = 2;
+        private const int cmd_nextSelectedScene = 18;
+        private const int cmd_currentSelectedScene = 17;
         private bool instaChangeActive;
 
         private int currentTick;
         private float transitionLength = 16; //sets the duration in Seconds, how long a transition has to be in "TimeLine" to be played back correctly when CYLVESTER is hooked up correctly
         private float restTimeS = 1f; //init transTime is 1 Second
 
-        [SerializeField] private int lastSelectedScene = 0;
-        [SerializeField] private int currentSelectedScene = 0;
+        [SerializeField] private int currentSelectedScene;
         [SerializeField] private int nextSelectedScene;
 
         [SerializeField, Range(1, 16)] private int channel = 1;
@@ -41,13 +40,29 @@ namespace cylvester
             if (mes.Status - 176 == channel - 1) //Which Channel
             {
 
-                if (mes.Data1 == nextSelectedSceneOutput)
+                if (mes.Data1 == cmd_nextSelectedScene)
                 {
                     nextSelectedScene = mes.Data2; //Get next Schene Update
                 }
 
-                if (mes.Data1 == lastSelectedSceneOutput)
-                    lastSelectedScene = mes.Data2; //Get current selected Scene
+                if (mes.Data1 == cmd_instaTrig)
+                {
+                    instaChangeActive = true;
+                }
+
+                if (mes.Data1 == cmd_currentSelectedScene)
+                { 
+                    currentSelectedScene = mes.Data2; //Get current selected Scene
+
+                    if (instaChangeActive)
+                    {
+                        stateManager.SelectedState = currentSelectedScene;
+                        playableDirector.playableGraph.GetRootPlayable(0).SetSpeed(10);
+                        //                        Debug.Log("Instatrig " + currentSelectedScene);
+                        //                        Debug.Log("Last selected Scene new " + lastSelectedScene);
+                        instaChangeActive = false;
+                    }
+                }
 
                 if (mes.Data1 == oneBarLoopButton) //Button fourBarLoop
                 {
@@ -63,21 +78,6 @@ namespace cylvester
                     stateManager.SelectedState = nextSelectedScene;
                 }
 
-                if (mes.Data1 == instaTrig)
-                {
-                    instaChangeActive = true;
-
-                    if (instaChangeActive && (currentSelectedScene != lastSelectedScene))
-                    {
-                        currentSelectedScene = lastSelectedScene;
-                        stateManager.SelectedState = lastSelectedScene;
-                        playableDirector.playableGraph.GetRootPlayable(0).SetSpeed(10);
-                        Debug.Log("Instatrig " + currentSelectedScene);
-                        Debug.Log("Last selected Scene new " + lastSelectedScene);
-                        instaChangeActive = false;
-                    }
-                    
-                }
             }
         }
 
