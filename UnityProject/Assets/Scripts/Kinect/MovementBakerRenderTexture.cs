d9aaff1f218dce56af441a032879696cc0481141
Chikashi Miyama <chikashimiyama@gmail.com>
2019-12-12 17:48:03 +0100

    implement #136

---
+++ UnityProject/Assets/Scripts/Kinect/MovementBakerRenderTexture.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace cylvester
+{
+
+    public class MovementBakerRenderTexture : MonoBehaviour
+    {
+        [SerializeField] private RenderTexture input;
+        [SerializeField] private ComputeShader computeShader;
+        [SerializeField] private float factor = 1;
+
+        private RenderTexture resultTexture_;
+        private RenderTexture previousFrameTexture_;
+        private int kernelHandle_;
+        private Renderer renderer_;
+
+        private void Start()
+        {
+            resultTexture_ = new RenderTexture(input.width, input.height, 0, input.graphicsFormat) { enableRandomWrite = true };
+            resultTexture_.Create();
+
+            previousFrameTexture_ = new RenderTexture(input.width, input.height, 0, input.graphicsFormat) { enableRandomWrite = true };
+            previousFrameTexture_.Create();
+
+            kernelHandle_ = computeShader.FindKernel("CSMain");
+
+            renderer_ = GetComponent<Renderer>();
+        }
+
+        private void Update()
+        {
+            computeShader.SetTexture(kernelHandle_, "Input", input);
+            computeShader.SetTexture(kernelHandle_, "Previous", previousFrameTexture_);
+            computeShader.SetTexture(kernelHandle_, "Result", resultTexture_);
+            computeShader.SetFloat("Factor", factor);
+
+            computeShader.Dispatch(kernelHandle_, 1, 1, 1);
+
+            renderer_.material.SetTexture("_differenceTexture", resultTexture_);
+
+        }
+    }
+
+}
+
+
