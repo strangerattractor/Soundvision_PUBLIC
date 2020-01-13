6985cd74610a7fce9c96c0dcda4211d2c8ba2627
max <mschweder@gmail.com>
2020-01-13 16:20:47 +0100

    Added First Project

---
+++ UnityProject/Assets/Scenes/Examples/Max Silly Demos/Scripts/LightMapper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightMapper : MonoBehaviour
+{
+    [SerializeField] private Light light;
+    [SerializeField] private float minimum = 100;
+    [SerializeField] private float multiplier = 100;
+
+    public void OnEnergyChanged(float energy)
+    {
+        light.intensity = energy * multiplier + minimum;
+    }
+}
+
