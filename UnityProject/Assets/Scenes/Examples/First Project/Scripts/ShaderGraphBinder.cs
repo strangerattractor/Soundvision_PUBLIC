bb90aabba15af8d83debb3c1e73261939dab623e
max <mschweder@gmail.com>
2020-01-14 20:34:58 +0100

    Understood Depth of field. Optimized Particle Count VFX Graph

---
+++ UnityProject/Assets/Scenes/Examples/First Project/Scripts/ShaderGraphBinder.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderGraphBinder : MonoBehaviour
+{
+    [SerializeField] private Material material_;
+    [SerializeField] private string valueName_ = "ValueName_";
+
+    public void OnEnergyChanged(float value)
+    {
+        material_.SetFloat(valueName_, value * .1f);
+    }
+}
