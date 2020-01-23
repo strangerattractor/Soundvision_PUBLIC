4e698ea2cbd043df86381bf0a8938a3ba3617680
max <mschweder@gmail.com>
2020-01-23 20:35:09 +0100

    Added AzureForces VFX

--- UnityProject/Assets/Scenes/Examples/Pitch VFX/PitchPosition.cs
+++ UnityProject/Assets/Scenes/Examples/Pitch VFX/PitchPosition.cs
@@ -9,6 +9,9 @@ namespace cylvester
     public class PitchPosition : MonoBehaviour
 {
        [SerializeField] float pitchMultiplier = 0.1f;
+        [SerializeField] float yOffset = 0;
+        [SerializeField] float xOffset = 0;
+        [SerializeField] float zOffset = 0;
        private Vector3 originalPosition;
 
         public void Start()
@@ -21,7 +24,9 @@ namespace cylvester
             set
             {
                 var pitchOffset = value * pitchMultiplier;
-                transform.localPosition = new Vector3(originalPosition.x, originalPosition.y, originalPosition.z + pitchOffset);
+                transform.localPosition = new Vector3(originalPosition.x + (pitchOffset * xOffset),
+                    originalPosition.y + (pitchOffset * yOffset),
+                    originalPosition.z + (pitchOffset + zOffset));
             }
         }
     }
