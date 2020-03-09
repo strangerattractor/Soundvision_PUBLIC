2d7951cd687d62899e4cfe6e2986b237d31e3723
max <mschweder@gmail.com>
2020-03-09 11:06:17 +0100

    Added Second Screen Script

---
+++ UnityProject/Assets/Scripts/SecondScreen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SecondScreen : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("displays connected: " + Display.displays.Length);
+
+        for (int i = 1; i < Display.displays.Length; i++)
+        {
+            Display.displays[i].Activate();
+        }
+    }
+}
