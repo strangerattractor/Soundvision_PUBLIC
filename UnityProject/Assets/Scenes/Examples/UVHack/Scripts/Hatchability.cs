7ce43c480ff0d1a68d3e93d3c9d5e26270d81af9
naoto <micuat@gmail.com>
2020-03-10 15:54:27 +0100

    added uvhack scene for uv coord testing

---
+++ UnityProject/Assets/Scenes/Examples/UVHack/Scripts/Hatchability.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using cylvester;
+
+public class Hatchability : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject seed;
+    List<GameObject> gameObjects = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int N = 2;
+        for (int i = -N; i <= N; i++)
+        {
+            for (int j = -N; j <= N; j++)
+            {
+                var gp = new GameObject("node");
+                gp.transform.parent = transform;
+                gp.transform.position = new Vector3(j, i, 0);
+                var g = Instantiate(seed, gp.transform);
+                g.GetComponent<UVMapToScreen>().cam = cam;
+                g.GetComponent<UVMapToScreen>().amount = 0.02f;
+                gameObjects.Add(g);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //foreach(var g in gameObjects)
+        //{
+        //    g.transform.eulerAngles = new Vector3(0, Time.time * 10, 0);
+        //}
+    }
+
+    public void OnTriggerReceived()
+    {
+        int index = (int)Mathf.Floor(Random.Range(0, gameObjects.Count));
+        gameObjects[index].GetComponent<cylvester.OrangeCube>().Invoke("OnTriggerReceived", 0);
+    }
+
+    public void OnStateChanged(IStateReader currentState)
+    {
+    }
+}
---
+++ UnityProject/Assets/Scenes/Examples/UVHack/Scripts/UVMapToScreen.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UVMapToScreen : MonoBehaviour
+{
+    public Camera cam;
+    public float amount = 0;
+    public bool correctSquare = true;
+
+    void CorrectUVMap(float lerpRate)
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        Vector2[] uv = mesh.uv;
+
+        float longSide = Mathf.Max(cam.scaledPixelWidth, cam.scaledPixelHeight);
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            //var u = cam.WorldToViewportPoint(transform.TransformPoint(vertices[i]));
+            var u = cam.WorldToScreenPoint(transform.TransformPoint(vertices[i]));
+            if (correctSquare)
+            {
+                u.x -= cam.scaledPixelWidth * 0.5f;
+                u.y -= cam.scaledPixelHeight * 0.5f;
+                u.x /= longSide;
+                u.y /= longSide;
+                u.x += 0.5f;
+                u.y += 0.5f;
+            }
+            else
+            {
+                u.x /= cam.scaledPixelWidth;
+                u.y /= cam.scaledPixelHeight;
+            }
+            uv[i] = new Vector2(Mathf.Lerp(uv[i].x, u.x, lerpRate), Mathf.Lerp(uv[i].y, u.y, lerpRate));
+        }
+
+        mesh.uv = uv;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CorrectUVMap(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CorrectUVMap(amount);
+    }
+}
