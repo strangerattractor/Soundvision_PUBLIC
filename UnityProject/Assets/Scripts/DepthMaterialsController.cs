6e877fa0e5ef4bae9c9603e98d055109fdb75123
max <mschweder@gmail.com>
2020-03-18 18:34:42 +0100

    Fixed CameraDepth Shader

--- UnityProject/Assets/Scripts/DepthMaterialsController.cs
+++ UnityProject/Assets/Scripts/DepthMaterialsController.cs
@@ -23,6 +23,18 @@ public class DepthMaterialsController : MonoBehaviour
     private static readonly int DisplacementFactor_ = Shader.PropertyToID("_DisplacementFactor");
     [SerializeField] float displacementFactor = 10f;
 
+    private static readonly int DampingFactor_ = Shader.PropertyToID("_DampingFactor");
+    [SerializeField, Range(0.0f, 1.0f)] float dampingFactor = .5f;
+
+    private static readonly int BlendScanTexture_ = Shader.PropertyToID("_Blend_Scan_Texture");
+    [SerializeField, Range(0.0f, 1.0f)] float blendScanTexture = .5f;
+
+    private static readonly int ScanGridSize_ = Shader.PropertyToID("_ScanGridSize");
+    [SerializeField, Range(0.0f, 1.0f)] float scanGridSize = .8f;
+
+    private static readonly int ScanGridTiling_ = Shader.PropertyToID("_ScanGridTiling");
+    [SerializeField] Vector2 scanGridTiling = new Vector2(.8f, .8f);
+
     private void Start()
     {
         depthTextureRenderer_ = materials.GetComponent<Renderer>();
@@ -38,6 +50,10 @@ public class DepthMaterialsController : MonoBehaviour
             material.SetFloat(EmissionMultiplier_, emissionMultiplier);
             material.SetFloat(YTiling_, yTiling);
             material.SetFloat(DisplacementFactor_, displacementFactor);
+            material.SetFloat(DampingFactor_, dampingFactor);
+            material.SetFloat(BlendScanTexture_, blendScanTexture);
+            material.SetFloat(ScanGridSize_, scanGridSize);
+            material.SetVector(ScanGridTiling_, scanGridTiling);
         }
     }
 }
