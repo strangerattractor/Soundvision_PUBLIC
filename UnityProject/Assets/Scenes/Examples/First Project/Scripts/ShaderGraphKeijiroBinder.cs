ec5aaba382b71ee15694892bb6a95ac422e06c62
naoto <micuat@gmail.com>
2020-03-19 19:02:48 +0100

    vfx/shader keijiro binder

---
+++ UnityProject/Assets/Scenes/Examples/First Project/Scripts/ShaderGraphKeijiroBinder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderGraphKeijiroBinder : MonoBehaviour
+{
+    [SerializeField] private Material material_;
+    [SerializeField] private string valueName_ = "ValueName_";
+
+    private float _val;
+
+    public float val
+    {
+        get => _val;
+        set => _val = value;
+    }
+
+    void Update()
+    {
+        material_.SetFloat(valueName_, _val);
+    }
+}
---
+++ UnityProject/Assets/Scenes/Examples/First Project/Scripts/VFXKeijiroBinder.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.VFX;
+
+namespace cylvester
+{ 
+    public class VFXKeijiroBinder : MonoBehaviour
+    {
+        [SerializeField] private VisualEffect targetVFX_ = null;
+        [SerializeField] private string valueName_ = "Value_1";
+        private float _val;
+
+        public float val
+        {
+            get => _val;
+            set => _val = value;
+        }
+
+        void Update()
+        {
+            targetVFX_.SetFloat(valueName_, _val);
+        }
+    }
+}
