0490377f200fa7a724af515c6e4f2b9e7daef303
max <mschweder@gmail.com>
2020-04-05 11:23:22 +0200

    Added Terrain

---
+++ UnityProject/Assets/Scripts/PdConnection/Lissajous.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace cylvester
+{
+    public interface ILissajous
+    {
+        Texture2D Texture { get; }
+    }
+
+
+    public class Lissajous : PdBaseBind, ILissajous
+    {
+        [SerializeField] private RenderTexture renderTexture;
+        
+        private IPdArraySelector waveformArraySelectorLeft_;
+        private IPdArraySelector waveformArraySelectorRight_;
+        private Texture2D texture_;
+        private int[] cache_;
+        
+        void Start()
+        {
+            base.Start();
+            cache_ = new int[PdConstant.BlockSize];
+            waveformArraySelectorLeft_ = new PdArraySelector(pdbackend.WaveformArrayContainer);
+            waveformArraySelectorRight_ = new PdArraySelector(pdbackend.WaveformArrayContainer);
+
+            texture_ = new Texture2D(PdConstant.BlockSize, PdConstant.BlockSize, TextureFormat.R8, false);
+            
+            var pixels = texture_.GetPixels();
+            for (var i = 0;i < pixels.Length; ++i)
+                pixels[i] = Color.black;
+            texture_.SetPixels(pixels);
+            texture_.Apply();
+        }
+
+        void Update()
+        {
+            waveformArraySelectorLeft_.Selection = channel - 1;
+            waveformArraySelectorRight_.Selection = channel - 1;
+
+            var arrayLeft = waveformArraySelectorLeft_.SelectedArray;
+            var arrayRight = waveformArraySelectorRight_.SelectedArray;
+
+            for (var i = 0; i < PdConstant.BlockSize; i++)
+            {
+                var x = (int)(256f * Mathf.Clamp(arrayLeft[i], -1f, 1f)) + 256;
+                var y = (int)(256f * Mathf.Clamp(arrayRight[i], -1f, 1f)) + 256;
+                texture_.SetPixel(i , cache_[i], Color.black);
+                texture_.SetPixel(x , y, Color.white);
+                cache_[i] = y;
+            }
+
+            texture_.Apply();
+            if(renderTexture != null)
+            {
+                Graphics.Blit(texture_, renderTexture);
+            }
+        }
+
+        public Texture2D Texture => texture_;
+    }
+}
